ceb8fd20abf370a4569f0bca1d0ac78b3c7efff3
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-01-17 01:37:46 -0300

    fix: minor business logics adjustments

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,7 +5,7 @@ import { Providers } from "@/components/Providers";
 import { ProfileForm } from "./components";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function Search() {
+export default function Profile() {
   useRedirectToLoginPage();
 
   return (
