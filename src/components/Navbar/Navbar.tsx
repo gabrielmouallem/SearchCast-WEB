b23b78385a508f30b907da365693801c75453302
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-07-23 21:20:01 -0300

    feat: added blur to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ interface NavbarProps {
 
 export function Navbar({ isAuthenticated, restrictedMode }: NavbarProps) {
   return (
-    <nav className="fixed inset-0 z-50 flex h-20 w-full items-center bg-gradient-to-b from-black-transparent to-transparent sm:pl-20 sm:pr-20">
+    <nav className="fixed inset-0 z-50 flex h-20 w-full items-center bg-gradient-to-b from-black-transparent to-transparent backdrop-blur-[1px] sm:pl-20 sm:pr-20">
       <a href="/">
         <Image
           className="ml-8"
