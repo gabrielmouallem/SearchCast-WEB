9766e86292ce93e1e92176dcc87444a0392c28e5
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-07-25 09:57:42 -0300

    feat: robots.txt generated

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { getAppURL } from "@/utils/getAppURL";
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `${getAppURL()}/sitemap`,
+  };
+}
---
+++ src/utils/getAppURL.ts
@@ -0,0 +1,3 @@
+export function getAppURL() {
+  return process.env.NEXT_PUBLIC_APP_URL;
+}
