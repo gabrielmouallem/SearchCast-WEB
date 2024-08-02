c67555d24218221b918943f24fcd8ed680b25dd4
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-08-01 21:17:00 -0300

    feat: adjusted navbar logo redirect when is authenticated

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,7 +19,7 @@ interface NavbarProps {
 export function Navbar({ isAuthenticated, restrictedMode }: NavbarProps) {
   return (
     <nav className="fixed inset-0 z-50 flex h-20 w-full items-center bg-gradient-to-b from-black-transparent to-transparent backdrop-blur-[1px] sm:pl-20 sm:pr-20">
-      <a href="/">
+      <a href={isAuthenticated ? "/search" : "/"}>
         <Image
           className="ml-8"
           src="/logo_w_name.svg"
