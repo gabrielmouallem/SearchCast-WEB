e53b1260ff08275fe04da3512d8f590b7be2c787
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-08-26 19:17:04 -0300

    feat: added posthog to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import { Navbar, Footer } from "@/components";
 import { PageContent } from "@/components/Content/PageContent";
+import { Providers } from "@/components/Providers";
 import { getAndIdentifyUser } from "@/utils/server/getAndIdentifyUser";
 
 export default async function Home() {
   await getAndIdentifyUser();
 
   return (
-    <main className="pt-5 md:pt-16">
-      <Navbar />
-      <PageContent />
-      <Footer />
-    </main>
+    <Providers>
+      <main className="pt-5 md:pt-16">
+        <Navbar />
+        <PageContent />
+        <Footer />
+      </main>
+    </Providers>
   );
 }
