340770c0af1639e0c5250d851f915734167b9624
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-10-01 19:57:01 -0300

    feat: implemented onboading and redirect to plans flow

--- src/middlewares/clientAuthMiddleware.ts
+++ src/middlewares/clientAuthMiddleware.ts
@@ -1,7 +1,9 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { Paths, PROTECTED_PATHS, PUBLIC_PATHS } from "@/constants/paths";
+import { getActiveSubscription } from "@/app/plans/components/PlanCard/actions/getActiveSubscription";
+import { createClient } from "@/services/server";
 
-export function handleClientAuthMiddleware(
+export async function handleClientAuthMiddleware(
   request: NextRequest,
   pathname: string,
 ) {
@@ -21,6 +23,39 @@ export function handleClientAuthMiddleware(
     if (PUBLIC_PATHS.some((path) => pathname === path)) {
       // Redirect authenticated users to search page if they try to access login or register
       return NextResponse.redirect(new URL(Paths.SEARCH, request.url));
+    }
+
+    // Initialize Supabase client
+    const supabase = createClient();
+
+    // Get user data
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser(accessToken);
+
+    if (error) {
+      console.error("Error fetching user:", error);
+      return NextResponse.next();
+    }
+
+    if (user) {
+      const createdAt = new Date(user.created_at);
+      const now = new Date();
+      const isFirstAccess = now.getTime() - createdAt.getTime() < 5 * 60 * 1000; // 5 minutes
+
+      // Condition for redirecting from search to plans (for non-first-time users)
+      if (pathname === Paths.SEARCH) {
+        if (isFirstAccess) {
+          // Redirect first-time users to the onboarding page
+          return NextResponse.redirect(new URL("/onboarding", request.url));
+        }
+
+        const activeSubscription = await getActiveSubscription(user.email!);
+        if (!activeSubscription && !user.user_metadata?.allow_unpaid_access) {
+          return NextResponse.redirect(new URL("/plans", request.url));
+        }
+      }
     }
   }
 
