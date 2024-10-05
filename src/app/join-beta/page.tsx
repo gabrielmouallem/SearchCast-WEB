40937d2da2308d413b61baac89f7a3dab91194dd
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-10-05 16:07:11 -0300

    fix: page name

--- src/app/join-beta/page.tsx
+++ src/app/join-beta/page.tsx
@@ -1,7 +1,7 @@
 import { Providers } from "@/components/Providers";
 import { Beta } from "./components/Beta";
 
-export default async function PlansPage() {
+export default async function BetaPage() {
   return (
     <Providers>
       <Beta />
