d7cd79fa9323a94db59f63642b1dec99f2995f67
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-10-07 15:27:25 -0300

    fix: redudant preffix

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -14,7 +14,7 @@ import React from "react";
 Bugsnag.start({
   apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
   plugins: [new BugsnagPluginReact()],
-  appVersion: `searchcast-${process.env.NEXT_PUBLIC_BUILD_ID}`,
+  appVersion: process.env.NEXT_PUBLIC_BUILD_ID,
   releaseStage:
     process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
       ? "production"
