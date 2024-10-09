34dce575fb7d2ff160f89cf365e36aca0f5e6a95
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-10-09 20:06:23 -0300

    fix: removed test error

--- src/components/SearchEngine/SearchEngine.tsx
+++ src/components/SearchEngine/SearchEngine.tsx
@@ -5,7 +5,7 @@ import Lottie from "lottie-react";
 import Image from "next/image";
 import { getLastUpdate } from "@/utils/shared";
 import logoLottieFile from "../../../public/logo_lottie_animation.json";
-import React, { useCallback, useEffect, useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import {
   SearchOrderBy,
   SearchResultItem,
@@ -19,7 +19,6 @@ import { LoadingFallback } from "../LoadingFallback";
 import { OrderByValue } from "@/types";
 import { ORDER_BY_VALUE_TO_LABEL_MAP } from "../SearchOrderBy/SearchOrderBy.constants";
 import { CopyToClipboard } from "../CopyToClipboard";
-import Bugsnag from "@bugsnag/js";
 
 interface SearchEngineProps {
   options?: {
@@ -90,11 +89,6 @@ export function SearchEngine({ options }: SearchEngineProps) {
   const getLogoDisplayStyle = (show: boolean) => ({
     display: show ? "initial" : "none",
   });
-
-  useEffect(() => {
-    const timestamp = new Date().toLocaleString();
-    Bugsnag.notify(new Error(`Test error finale at ${timestamp}`));
-  }, []);
 
   if (!isMounted)
     return <LoadingFallback height="[height:calc(100vh-100px)]" />;
